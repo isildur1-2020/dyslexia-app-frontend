849c554944cba5a72dabf048534a6f30e00460e8
tapitarias <isildur1@tapitariass-MacBook-Air.local>
2022-07-11 23:09:13 -0500

    add build dir

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BACK_URL = "https://localhost:8088";
-// export const BACK_URL = "https://dyslexia-app2.herokuapp.com";
+// export const BACK_URL = "https://localhost:8088";
+export const BACK_URL = "http://82.180.139.169:62345";
